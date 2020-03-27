41b7088253c2f5c09d36dd47a75a91c2e8a18be8
Andrew Chou <andrewchou@Andrews-MacBook-Pro.local>
2020-03-27 11:36:47 -0700

    feed btn patch

--- client/src/components/FeedBtn.js
+++ client/src/components/FeedBtn.js
@@ -9,11 +9,9 @@ export default (props) => {
 
     const clickHandler = (e, id) => {
         e.preventDefault();
-        if(id==!undefined){
-            axios.put(`http://localhost:8000/api/neoGotchi/${id}/edit`, {status:{hunger:Math.floor(props.neogotchi.status.hunger+randomVal(20)), happiness:Math.floor(props.neogotchi.status.happiness+randomVal(20))}})
+        axios.put(`http://localhost:8000/api/neoGotchi/${id}/edit`, {status:{hunger:Math.floor(props.neogotchi.status.hunger+randomVal(20)), happiness:Math.floor(props.neogotchi.status.happiness+randomVal(20))}})
             .then(res => console.log(res))
             .catch(err => console.log(err));
-        }
         // add user purchasing food after testing is completed
     }
 
